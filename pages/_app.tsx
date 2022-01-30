8a211cc5cae816747d7f43ede32e9a9c7b4a4818
Vladislav Vavilikhin (contractor) <vladislav.vavilikhin@macys.com>
2022-01-30 20:30:39 +0300

    chore: mui setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { FC } from 'react';
+import type { AppProps } from 'next/app';
+import CssBaseline from '@mui/material/CssBaseline';
+import { EmotionCache } from '@emotion/cache';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import theme from '../styles/theme/main';
+import createEmotionCache from '../utils/createEmotionCache';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const clientSideEmotionCache = createEmotionCache();
 
-export default MyApp
+const App: FC<AppProps & { emotionCache: EmotionCache }> = ({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default App;
---
+++ pages/_document.tsx
@@ -0,0 +1,75 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import theme from '../styles/theme/main';
+import createEmotionCache from '../utils/createEmotionCache';
+import { AppType } from 'next/dist/shared/lib/utils';
+import { EmotionCache } from '@emotion/cache';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<
+    DocumentInitialProps & { emotionStyleTags: typeof emotionStyleTags }
+  > {
+    const originalRenderPage = ctx.renderPage;
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: AppType) =>
+          function EnhanceApp(props) {
+            // @ts-expect-error refactor
+            return <App emotionCache={cache} {...props} />;
+          },
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(' ')}`}
+        key={style.key}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ));
+
+    return {
+      ...initialProps,
+      emotionStyleTags,
+    };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name='theme-color' content={theme.palette.primary.main} />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+          />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/icon?family=Material+Icons'
+          />
+          {(this.props as any).emotionStyleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
